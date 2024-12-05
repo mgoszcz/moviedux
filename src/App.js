5ab079a05561f0e7ad5ce223261419fbb97f2835
Marcin Goszczynski <marcin.goszczynski@imgarena.com>
2024-12-05 13:52:02 +0100

    watchlisting, lifting state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,68 @@ import "./styles.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MoviesGrid from "./components/MoviesGrid";
+import Watchlist from "./components/Watchlist";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [watchlist, setWatchlist] = useState([]);
+
+  const toggleWatchList = (movieId) => {
+    setWatchlist((prev) =>
+      prev.includes(movieId)
+        ? prev.filter((id) => id !== movieId)
+        : [...prev, movieId],
+    ); // using previous state to check
+  };
+
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data);
+      });
+  });
+
   return (
     <div className="App">
       <div className={"container"}>
         <Header />
-        <MoviesGrid />
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <MoviesGrid
+                  movies={movies}
+                  watchlist={watchlist}
+                  toggleWatchlist={toggleWatchList}
+                />
+              }
+            />
+            <Route
+              path="/watchlist"
+              element={
+                <Watchlist
+                  movies={movies}
+                  watchlist={watchlist}
+                  toggleWatchlist={toggleWatchList}
+                />
+              }
+            />
+          </Routes>
+        </Router>
       </div>
       <Footer />
     </div>
---
+++ src/components/Watchlist.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import "../styles.css";
+import MovieCard from "./MovieCard";
+
+export default function Watchlist({ movies, watchlist, toggleWatchlist }) {
+  return (
+    <div>
+      <h1 className={"title"}>Your watchlist</h1>
+      <div className={"watchlist"}>
+        {watchlist.map((id) => {
+          const movie = movies.find((movie) => movie.id === id);
+          return (
+            <MovieCard
+              key={id}
+              movie={movie}
+              toggleWatchlist={toggleWatchlist}
+              isWatchlisted={true}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
